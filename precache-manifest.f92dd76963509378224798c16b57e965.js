ae0ce3b7d8182da24d3123ace59a6ad78a79e9bb
王镇佳 <wangzhenjia@bytedance.com>
2020-09-21 23:29:48 +0800

    Updates

---
+++ precache-manifest.f92dd76963509378224798c16b57e965.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a437a3df8db34a907b8c4257343b09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f1374798f0d46340c14",
+    "url": "/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "119b02064ee43f52c392",
+    "url": "/static/css/main.c82fa903.chunk.css"
+  },
+  {
+    "revision": "4f1374798f0d46340c14",
+    "url": "/static/js/2.2db7179d.chunk.js"
+  },
+  {
+    "revision": "3d669e94f91837166de5dcb402c232d1",
+    "url": "/static/js/2.2db7179d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119b02064ee43f52c392",
+    "url": "/static/js/main.a5e3fa97.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
