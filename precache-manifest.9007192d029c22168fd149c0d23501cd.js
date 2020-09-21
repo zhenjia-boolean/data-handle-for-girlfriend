dc01a317460f6fd77075a6e3b7f01451dbdd108e
王镇佳 <wangzhenjia@bytedance.com>
2020-09-21 23:43:54 +0800

    Updates

---
+++ precache-manifest.9007192d029c22168fd149c0d23501cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed79f9bb4ef1eec216a05ba84dc736b",
+    "url": "/data-handle-for-girlfriend/index.html"
+  },
+  {
+    "revision": "4f1374798f0d46340c14",
+    "url": "/data-handle-for-girlfriend/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "ba0dcd32e2fc83432813",
+    "url": "/data-handle-for-girlfriend/static/css/main.c82fa903.chunk.css"
+  },
+  {
+    "revision": "4f1374798f0d46340c14",
+    "url": "/data-handle-for-girlfriend/static/js/2.2db7179d.chunk.js"
+  },
+  {
+    "revision": "3d669e94f91837166de5dcb402c232d1",
+    "url": "/data-handle-for-girlfriend/static/js/2.2db7179d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0dcd32e2fc83432813",
+    "url": "/data-handle-for-girlfriend/static/js/main.bcd14842.chunk.js"
+  },
+  {
+    "revision": "e3ac459d3c54bd5b1ae2",
+    "url": "/data-handle-for-girlfriend/static/js/runtime-main.7fd197a6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.49e1a49e47f3aadf9ec1bb6ea146dafa.js"
+  "/data-handle-for-girlfriend/precache-manifest.9007192d029c22168fd149c0d23501cd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/data-handle-for-girlfriend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
